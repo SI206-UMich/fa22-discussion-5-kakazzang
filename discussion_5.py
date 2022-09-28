5a1d6bf5544f387230ef78b933e168b398a72220
Jingting Wang <jingtw@umich.edu>
2022-09-28 17:53:51 -0400

    add function

--- discussion_5.py
+++ discussion_5.py
@@ -42,23 +42,25 @@ class Warehouse:
 
 	# Returns the item in the warehouse with the most stock		
 	def get_max_stock(self):
-		most_stock = ''
+		most_stock = 0
+		most_item = None
 		for i in range(len(self.items)):
 			current_item = self.items[i]
 			current_stock = current_item.stock
 			if current_stock > most_stock:
-				most_stock = current_item
-		return most_stock.name
-	
+				most_stock = current_stock
+				most_item = current_item
+		return most_item
+
 	# Returns the item in the warehouse with the highest price
 	def get_max_price(self):
-		highest_price = 0	
-		for i in range(len(self.items)):
-			current_item = self.items[i]
-			current_price = current_item.price
-			if current_price > highest_price:
-				highest_price = current_item
-		return highest_price
+		highest_price = 0
+		highest_item = None	
+		for item in self.items:
+			if item.price > highest_price:
+				highest_price = item.price
+				highest_item = item
+		return highest_item
 
 # Tests
 class TestAllMethods(unittest.TestCase):
@@ -77,26 +79,41 @@ class TestAllMethods(unittest.TestCase):
 		self.assertEqual(total_a1, 1)
 		total_a2 = count_a('ahahah')
 		self.assertEqual(total_a2, 3)
-
+		total_a3 = count_a('bbb')
+		self.assertEqual(total_a3, 0)
 
 	## Check to see whether you can add an item to the warehouse
 	def test_add_item(self):
-		# Warehouse.add_item(self.item1, self.item2)
-		# actual= Warehouse.print_items()
-		# expect = {["Beer", "Cider"]}
-		# self.assertEqual(actual, expect)
-		pass
+		w1 = Warehouse()
+		w1.add_item(self.item1)
+		w1.add_item(self.item2)
+		self.assertEqual(w1.items, [self.item1, self.item2])
+		w1.add_item(self.item3)
+		self.assertEqual(w1.items, [self.item1, self.item2, self.item3])
+		w1.add_item(self.item4)
+		self.assertEqual(w1.items, [self.item1, self.item2, self.item3, self.item4])
+		
 
 	## Check to see whether warehouse correctly returns the item with the most stock
 	def test_warehouse_max_stocks(self):
-		self.stock_list = [self.item1, self.item2, self.item3, self.item4]
-		max_stock = Warehouse.get_max_stock(self.stock_list)
-		expect = "Water"
-		self.assertEqual(max_stock, expect)
+		w1 = Warehouse()
+		w1.items = [self.item1, self.item2, self.item3, self.item4]
+		self.assertEqual(w1.get_max_stock(), self.item3)
+		w1.items = [self.item5, self.item2, self.item4]
+		self.assertEqual(w1.get_max_stock(), self.item4)
+		w1.items = [self.item1, self.item2]
+		self.assertEqual(w1.get_max_stock(), self.item2)
+		
 
 	# Check to see whether the warehouse correctly return the item with the highest price
 	def test_warehouse_max_price(self):
-		pass
+		w1 = Warehouse()
+		w1.items = [self.item1, self.item2, self.item3, self.item4]
+		self.assertEqual(w1.get_max_price(), self.item1)
+		w1.items = [self.item5, self.item2, self.item4]
+		self.assertEqual(w1.get_max_price(), self.item2)
+		w1.items = [self.item1, self.item2]
+		self.assertEqual(w1.get_max_price(), self.item1)
 		
 
 def main():
